90c9e1b4a10792de11a75d16d74e26892d462ddb
abhi1d <5h1dabhi@gmail.com>
2018-04-24 02:32:52 +0530

    Added JDBC Auth

--- src/main/java/com/luv2code/springsecurity/demo/config/DemoSecurityConfig.java
+++ src/main/java/com/luv2code/springsecurity/demo/config/DemoSecurityConfig.java
@@ -1,22 +1,29 @@
 package com.luv2code.springsecurity.demo.config;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.User.UserBuilder;
 
 
 @Configuration
 @EnableWebSecurity
 public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
 
+	// add a reference to our security data source
+	
+	@Autowired
+	private DataSource securityDataSource;
+	
+	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		
-		// add our users for in memory authentication
+		/*// add our users for in memory authentication
 		
 		 UserBuilder users = User.withDefaultPasswordEncoder();
 	
@@ -24,6 +31,10 @@ public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
 		     .withUser(users.username("abhi").password("test123").roles("EMPLOYEE"))
 		     .withUser(users.username("marry").password("test123").roles("MANAGER","EMPLOYEE"))
 		     .withUser(users.username("saurabh").password("test123").roles("ADMIN","EMPLOYEE"));
+*/	
+	 auth.jdbcAuthentication().dataSource(securityDataSource);	
+	
+	
 	}
 
 	@Override
